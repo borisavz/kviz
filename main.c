c27a1d993f975b8492613ff3fd77ed7b2d7cc30e
Borisav Živanović <borisavzivanovic@gmail.com>
2016-04-20 20:51:21 +0200

    Kviz v1.5
    Igračima se može dodeliti ime.

--- main.c
+++ main.c
@@ -22,6 +22,14 @@ int main() {
     fgets(linija, 200, pitanja);
     printf("Unesi broj igraca: ");
     scanf("%d", &broj_igraca);
+    getchar();
+    char ime[broj_igraca][11];
+    puts("Unesi ime:");
+    for(i = 0; i < broj_igraca; i++) {
+        printf("\tIgrac %d: ", i + 1);
+        fgets(ime[i], 11, stdin);
+        ime[i][strlen(ime[i]) - 1] = '\0';
+    }
     printf("-----\nKviz - %s-----\nUnesi 0 da prekines kviz\n\n", linija);
     fscanf(odgovori, "%d", &broj_pitanja);
     tacni = calloc(broj_igraca, sizeof(int));
@@ -34,20 +42,20 @@ int main() {
             printf("\t%d. %s", ii + 1, linija);
         }
         puts("\n----");
-        getchar();
         for(ii = 0; ii < broj_igraca; ii++) {
-            printf("Igrac %d: ", ii + 1);
+            printf("%s: ", ime[ii]);
             scanf("%d", &odgovor);
             if(odgovor == 0)
                 exit(0);
             if(odgovor == tacan_odgovor)
                 *(tacni + ii) += 1;
         }
+        getchar();
         printf("\nTacan odgovor je pod %d\n\n", tacan_odgovor);
     }
     puts("Rezultati:");
     for(ii = 0; ii < broj_igraca; ii++)
-        printf("\tIgrac %d: %d/%d (%.2f %%)\n", ii + 1, *(tacni + ii), broj_pitanja,
+        printf("\t%s: %d/%d (%.2f %%)\n", ime[ii], *(tacni + ii), broj_pitanja,
             ((float) *(tacni + ii) / broj_pitanja) * 100);
     fclose(pitanja);
     fclose(odgovori);
